ba9b74236d87830e386600fda4e5773b458d3816
Alexander Heinrich <alxhnr@users.noreply.github.com>
2020-12-19 20:10:07 +0100

    Add .clang-format

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,46 +2,43 @@
  * Main playground for testing code.
  */
 
-#include <iostream>
-#include <string_view>
 #include <SDL.h>
+#include <iostream>
 #include <memory>
+#include <string_view>
 
-namespace
-{
-void throwSDLError(std::string_view message)
-{
-  throw std::runtime_error{std::string{"Error: "}.append(message).append(": ").append(SDL_GetError())};
+namespace {
+void throwSDLError(std::string_view message) {
+  throw std::runtime_error{
+      std::string{"Error: "}.append(message).append(": ").append(SDL_GetError())};
 }
 
-/** Wrap the given pointer in a unique pointer using the matching SDL function for release. */
+/** Wrap the given pointer in a unique pointer using the matching SDL function
+ * for release. */
 template <typename T>
-auto wrapPointer(T *ptr) {
+auto wrapPointer(T* ptr) {
   struct Deleter {
-    void operator()(SDL_Window *window) { SDL_DestroyWindow(window); };
-    void operator()(SDL_Renderer *renderer) { SDL_DestroyRenderer(renderer); };
+    void operator()(SDL_Window* window) { SDL_DestroyWindow(window); };
+    void operator()(SDL_Renderer* renderer) { SDL_DestroyRenderer(renderer); };
   };
   return std::unique_ptr<T, Deleter>{ptr};
 }
-}
+}  // namespace
 
-int main()
-{
+int main() {
   struct Context {
     Context() {
-      if(SDL_Init(SDL_INIT_EVERYTHING) != 0){
+      if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
         throwSDLError("Failed to initialize SDL2");
       }
     }
 
-    ~Context() {
-      SDL_Quit();
-    }
+    ~Context() { SDL_Quit(); }
   } context{};
 
-  SDL_Window *raw_window = nullptr;
-  SDL_Renderer *raw_renderer = nullptr;
-  if(SDL_CreateWindowAndRenderer(1280, 800, 0, &raw_window, &raw_renderer) != 0){
+  SDL_Window* raw_window = nullptr;
+  SDL_Renderer* raw_renderer = nullptr;
+  if (SDL_CreateWindowAndRenderer(1280, 800, 0, &raw_window, &raw_renderer) != 0) {
     throwSDLError("Failed to create window and renderer");
   }
   auto window = wrapPointer(raw_window);
@@ -51,13 +48,10 @@ int main()
   SDL_RenderPresent(renderer.get());
 
   bool program_running = true;
-  while(program_running)
-  {
+  while (program_running) {
     SDL_Event event;
-    while(SDL_WaitEvent(&event))
-    {
-      if(event.type == SDL_QUIT)
-      {
+    while (SDL_WaitEvent(&event)) {
+      if (event.type == SDL_QUIT) {
         program_running = false;
         break;
       }
