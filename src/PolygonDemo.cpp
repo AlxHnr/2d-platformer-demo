12eda677534c492fdc5c2db4d2dd1297c4b14e0b
Alexander Heinrich <alxhnr@users.noreply.github.com>
2020-12-20 15:49:24 +0100

    Draw normals for each edge

--- src/PolygonDemo.cpp
+++ src/PolygonDemo.cpp
@@ -4,15 +4,28 @@
 
 #include "PolygonDemo.hpp"
 #include <SDL.h>
+#include <cmath>
 #include <nonstd/span.hpp>
 
 namespace {
-void renderPolygon(SDL_Renderer *renderer, nonstd::span<GameEngine::PolygonDemo::Point> points) {
-  SDL_SetRenderDrawColor(renderer, 180, 180, 255, 255);
+using Point = GameEngine::PolygonDemo::Point;
 
+Point normalize(const Point &point) {
+  const auto length = sqrt(point.x * point.x + point.y * point.y) / 35;
+  return {static_cast<int>(point.x / length), static_cast<int>(point.y / length)};
+}
+
+void renderPolygon(SDL_Renderer *renderer, nonstd::span<Point> points) {
   const auto *previous_point = &points.back();
   for (const auto &point : points) {
+    SDL_SetRenderDrawColor(renderer, 180, 180, 255, 255);
     SDL_RenderDrawLine(renderer, previous_point->x, previous_point->y, point.x, point.y);
+
+    const Point center{(previous_point->x + point.x) / 2, (previous_point->y + point.y) / 2};
+    const auto normal = normalize({point.y - previous_point->y, previous_point->x - point.x});
+    SDL_SetRenderDrawColor(renderer, 255, 180, 180, 255);
+    SDL_RenderDrawLine(renderer, center.x, center.y, center.x + normal.x, center.y + normal.y);
+
     previous_point = &point;
   }
 }
