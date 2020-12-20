b017febfeab9c5177b049f04510ff943beac7e3b
Alexander Heinrich <alxhnr@users.noreply.github.com>
2020-12-20 22:58:45 +0100

    Use glm for vector manipulation

--- src/PolygonDemo.cpp
+++ src/PolygonDemo.cpp
@@ -5,22 +5,19 @@
 #include "PolygonDemo.hpp"
 #include <SDL.h>
 #include <cmath>
+#include <glm/geometric.hpp>
 #include <nonstd/span.hpp>
 
 namespace {
-glm::vec2 normalize(const glm::vec2 &point) {
-  const auto length = sqrt(point.x * point.x + point.y * point.y) / 35;
-  return {static_cast<int>(point.x / length), static_cast<int>(point.y / length)};
-}
-
 void renderPolygon(SDL_Renderer *renderer, nonstd::span<glm::vec2> points) {
   const auto *previous_point = &points.back();
   for (const auto &point : points) {
     SDL_SetRenderDrawColor(renderer, 180, 180, 255, 255);
     SDL_RenderDrawLine(renderer, previous_point->x, previous_point->y, point.x, point.y);
 
-    const glm::vec2 center{(previous_point->x + point.x) / 2, (previous_point->y + point.y) / 2};
-    const auto normal = normalize({point.y - previous_point->y, previous_point->x - point.x});
+    const auto center = (*previous_point + point) / 2.0f;
+    const auto normal =
+        glm::normalize(glm::vec2{point.y - previous_point->y, previous_point->x - point.x}) * 35.0f;
     SDL_SetRenderDrawColor(renderer, 255, 180, 180, 255);
     SDL_RenderDrawLine(renderer, center.x, center.y, center.x + normal.x, center.y + normal.y);
 
@@ -40,14 +37,12 @@ void PolygonDemo::handleFrame(SDL_Renderer *renderer, const std::chrono::microse
 
   auto moved_rectangle = rectangle;
   for (auto &points : moved_rectangle) {
-    points.x += 1 * move_factor;
-    points.y += 1 * move_factor;
+    points += 1 * move_factor;
   }
 
   auto moved_triangle = triangle;
   for (auto &points : moved_triangle) {
-    points.x -= 1 * move_factor;
-    points.y -= 1 * move_factor;
+    points -= 1 * move_factor;
   }
 
   renderPolygon(renderer, moved_rectangle);
