3087042f024e24f057ba759b5257cef42e41667d
Alexander Heinrich <alxhnr@users.noreply.github.com>
2020-12-22 18:40:46 +0100

    Abort tests on SDL_assert() failure

--- test/Main.cpp
+++ test/Main.cpp
@@ -2,5 +2,13 @@
  * Contains the doctest main function.
  */
 
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#define DOCTEST_CONFIG_IMPLEMENT
 #include <doctest/doctest.h>
+
+#include <SDL_assert.h>
+
+int main(const int arg_count, char **args) {
+  SDL_SetAssertionHandler([](const SDL_AssertData *, void *) { return SDL_ASSERTION_ABORT; },
+                          nullptr);
+  return doctest::Context{arg_count, args}.run();
+}
