547cc57c99fa94026205aabb7a237491265d0b0f
Alexander Heinrich <alxhnr@users.noreply.github.com>
2020-12-26 00:22:26 +0100

    Remove placeholder code from demo

--- src/PolygonDemo.cpp
+++ src/PolygonDemo.cpp
@@ -12,93 +12,17 @@
 #include <optional>
 
 namespace {
-/** Collects all normal vectors from the given polygon.
- *
- * @param points Vertices of the polygon.
- *
- * @return Normal vectors for each side of the polygon.
- */
-std::vector<glm::vec2> collectNormals(nonstd::span<const glm::vec2> points) {
-  std::vector<glm::vec2> result{};
-
-  GameEngine::Geometry::forEachEdge(
-      points, [&](size_t, const glm::vec2 &start, const glm::vec2 &end) {
-        const auto normal = GameEngine::Geometry::computeNormalOfEdge(start, end);
-        result.push_back(normal);
-      });
-
-  return result;
-}
-
 void renderPolygon(SDL_Renderer *renderer, nonstd::span<const glm::vec2> points,
                    const bool collision) {
-  GameEngine::Geometry::forEachEdge(
-      points, [&](size_t, const glm::vec2 &start, const glm::vec2 &end) {
-        if (collision) {
-          SDL_SetRenderDrawColor(renderer, 255, 255, 100, 255);
-        } else {
-          SDL_SetRenderDrawColor(renderer, 180, 180, 255, 255);
-        }
-        SDL_RenderDrawLine(renderer, start.x, start.y, end.x, end.y);
-
-        const auto center = (start + end) / 2.0f;
-        const auto normal = GameEngine::Geometry::computeNormalOfEdge(start, end) * 35.0f;
-        SDL_SetRenderDrawColor(renderer, 255, 180, 180, 255);
-        SDL_RenderDrawLine(renderer, center.x, center.y, center.x + normal.x, center.y + normal.y);
-      });
-}
-
-struct ProjectedEdges {
-  float min;
-  float max;
-  float distance;
-};
-
-/** Projects all vertices in the given polygon onto the given axis and returns the minmax values.
- *
- * @param points Represents the polygon.
- * @param axis All points in the given polygon will be projected onto this.
- *
- * @return Min and max values of all vertices projected onto the specified axis.
- */
-ProjectedEdges projectPolygonToNormal(nonstd::span<const glm::vec2> points, const glm::vec2 &axis) {
-  const auto values = GameEngine::Geometry::projectVerticesOntoAxisMinMax(points, axis);
-  return {values.min, values.max, values.max - values.min};
-}
-
-std::optional<glm::vec2> collidePolygons(SDL_Renderer *renderer, nonstd::span<const glm::vec2> a,
-                                         nonstd::span<const glm::vec2> b, const int offset) {
-  const glm::vec2 screen_center{640, 400};
-  int iteration = 1;
-
-  for (const auto &normal : collectNormals(a)) {
-    const auto green_vector = normal * 1000.0f;
-    SDL_SetRenderDrawColor(renderer, 180, 255, 180, 255);
-    SDL_RenderDrawLine(renderer, screen_center.x, screen_center.y, screen_center.x + green_vector.x,
-                       screen_center.y + green_vector.y);
-    SDL_RenderDrawLine(renderer, screen_center.x, screen_center.y,
-                       screen_center.x + glm::length(green_vector), screen_center.y);
-
-    const auto a_edges = projectPolygonToNormal(a, normal);
-    const auto b_edges = projectPolygonToNormal(b, normal);
-
-    if (a_edges.min > b_edges.max || a_edges.max < b_edges.min) {
-      return std::nullopt;
-    }
-
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-    SDL_RenderDrawLine(renderer, screen_center.x, screen_center.y - iteration * 2 + offset,
-                       screen_center.x + a_edges.distance,
-                       screen_center.y - iteration * 2 + offset);
-    SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
-    SDL_RenderDrawLine(renderer, screen_center.x, screen_center.y + iteration * 2 + offset,
-                       screen_center.x + b_edges.distance,
-                       screen_center.y + iteration * 2 + offset);
-
-    iteration++;
+  if (collision) {
+    SDL_SetRenderDrawColor(renderer, 255, 255, 100, 255);
+  } else {
+    SDL_SetRenderDrawColor(renderer, 180, 180, 255, 255);
   }
-
-  return glm::vec2{};
+  GameEngine::Geometry::forEachEdge(points,
+                                    [&](size_t, const glm::vec2 &start, const glm::vec2 &end) {
+                                      SDL_RenderDrawLine(renderer, start.x, start.y, end.x, end.y);
+                                    });
 }
 } // namespace
 
@@ -136,9 +60,6 @@ void PolygonDemo::handleFrame(SDL_Renderer *renderer, const std::chrono::microse
         glm::translate(glm::vec3{-750.0f, -470.0f, 0.0f});
     point = glm::vec2{matrix * glm::vec4{point, 0.0f, 1.0f}};
   }
-
-  collidePolygons(renderer, rectangle, triangle, 0);
-  collidePolygons(renderer, triangle, rectangle, 50);
 
   ConvexBoundingPolygon rectangle_polygon{rectangle[0], rectangle[1], rectangle[2], rectangle[3]};
   ConvexBoundingPolygon triangle_polygon{triangle[0], triangle[1], triangle[2]};
