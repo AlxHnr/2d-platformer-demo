03ede5354aa099974c7d6163c37fe29fdb2e8bc7
Alexander Heinrich <alxhnr@users.noreply.github.com>
2020-12-26 18:04:04 +0100

    Add usage examples

--- src/Collision.hpp
+++ src/Collision.hpp
@@ -20,6 +20,25 @@ using ConvexPolygonView = nonstd::span<const glm::vec2>;
  *
  * @return Displacement vector (MTV) for moving polygon a out of polygon b. This function will
  * return nothing if no collision occurred.
+ *
+ * Usage examples:
+ *
+ * @code
+ * std::array my_triangle{
+ *     glm::vec2{575, 400},
+ *     glm::vec2{792, 515},
+ *     glm::vec2{870, 670},
+ * };
+ *
+ * std::array my_line{
+ *     glm::vec2{575, 400},
+ *     glm::vec2{870, 670},
+ * };
+ *
+ * if(checkCollision(my_triangle, my_line) {
+ *   ...
+ * }
+ * @endcode
  */
 std::optional<glm::vec2> checkCollision(ConvexPolygonView a, ConvexPolygonView b);
 } // namespace GameEngine
