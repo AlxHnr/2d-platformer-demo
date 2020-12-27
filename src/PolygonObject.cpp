92e225bd2d151ebe7b5ba4819d7dcae35032d455
Alexander Heinrich <alxhnr@users.noreply.github.com>
2020-12-28 00:02:58 +0100

    Generalize polygon object

---
+++ src/PolygonObject.cpp
@@ -0,0 +1,40 @@
+/** @file
+ * Implements an interactive polygon compatible with the physics engine.
+ */
+
+#include "PolygonObject.hpp"
+#include <glm/common.hpp>
+#include <glm/gtc/constants.hpp>
+#include <glm/gtx/rotate_vector.hpp>
+
+namespace GameEngine {
+PolygonObject::PolygonObject(std::initializer_list<glm::vec2> vertices) : vertices{vertices} {
+  position = vertices.size() == 0 ? glm::vec2{0, 0} : Geometry::computeCenter(vertices);
+  std::transform(vertices.begin(), vertices.end(), std::back_inserter(vertices_relative_to_center),
+                 [this](const glm::vec2 &vertex) { return vertex - position; });
+}
+
+void PolygonObject::rotate(const float angle) {
+  orientation = glm::mod(orientation + angle, glm::pi<float>() * 2);
+  recomputeBoundingBox();
+}
+
+const glm::vec2 &PolygonObject::getPosition() { return position; }
+
+void PolygonObject::setPosition(const glm::vec2 &new_position) {
+  position = new_position;
+  recomputeBoundingBox();
+}
+
+const glm::vec2 &PolygonObject::getVelocity() { return velocity; }
+
+void PolygonObject::setVelocity(const glm::vec2 &new_velocity) { velocity = new_velocity; }
+
+Geometry::ConvexPolygonView PolygonObject::getBoundingPolygon() { return vertices; }
+
+void PolygonObject::recomputeBoundingBox() {
+  std::transform(
+      vertices_relative_to_center.cbegin(), vertices_relative_to_center.cend(), vertices.begin(),
+      [this](const glm::vec2 &vertex) { return glm::rotate(vertex, orientation) + position; });
+}
+} // namespace GameEngine
---
+++ src/PolygonObject.hpp
@@ -0,0 +1,55 @@
+/** @file
+ * Declares an interactive polygon.
+ */
+
+#ifndef GAME_ENGINE_SRC_POLYGON_OBJECT_HPP
+#define GAME_ENGINE_SRC_POLYGON_OBJECT_HPP
+
+#include "PhysicsObject.hpp"
+#include <vector>
+
+namespace GameEngine {
+/** Basic convex polygon to which physics and collisions can be applied. */
+class PolygonObject : public PhysicsObject {
+public:
+  /** Construct a polygon from the given vertices.
+   *
+   * @param vertices Zero or more points representing a convex polygon. If no points are provided,
+   * it will behave like a non-existing dummy.
+   */
+  PolygonObject(std::initializer_list<glm::vec2> vertices);
+
+  /** Rotate this polygon around its center.
+   *
+   * @param angle Rotation in radians.
+   */
+  void rotate(float angle);
+
+  /* PhysicsObject interface functions. */
+  const glm::vec2 &getPosition() override;
+  void setPosition(const glm::vec2 &new_position) override;
+  const glm::vec2 &getVelocity() override;
+  void setVelocity(const glm::vec2 &new_velocity) override;
+  Geometry::ConvexPolygonView getBoundingPolygon() override;
+
+private:
+  /** Center of this object. */
+  glm::vec2 position;
+
+  /** Angle at which the polygon was rotated around its center. */
+  float orientation{0.0f};
+
+  /** Speed and direction of this object. */
+  glm::vec2 velocity{0.0f, 0.0f};
+
+  /** Vertices of the polygon in the game world used for collision detection. */
+  std::vector<glm::vec2> vertices;
+
+  /** Vertices relative to the center of the polygon. Used for transformations. */
+  std::vector<glm::vec2> vertices_relative_to_center;
+
+  void recomputeBoundingBox();
+};
+} // namespace GameEngine
+
+#endif
