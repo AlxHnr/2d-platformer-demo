8f23ec407efa27db977fe90873a9d3be2d495dab
Alexander Heinrich <alxhnr@users.noreply.github.com>
2020-12-27 21:30:10 +0100

    Add PhysicsObject

---
+++ src/PhysicsObject.hpp
@@ -0,0 +1,32 @@
+/** @file
+ * Contains the interface for objects usable by the physics engine.
+ */
+
+#ifndef GAME_ENGINE_SRC_PHYSICS_OBJECT_HPP
+#define GAME_ENGINE_SRC_PHYSICS_OBJECT_HPP
+
+#include "Collision.hpp"
+#include <glm/vec2.hpp>
+
+namespace GameEngine {
+/** Interface for objects which can interact via the physics engine. */
+class PhysicsObject {
+public:
+  /** @return Center of the object in the game world. */
+  virtual const glm::vec2 &getPosition() = 0;
+
+  /** @param new_position New position (center) of the object in the game world. */
+  virtual void setPosition(const glm::vec2 &new_position) = 0;
+
+  /** @return Velocity of the object. */
+  virtual const glm::vec2 &getVelocity() = 0;
+
+  /** @param new_velocity New velocity of the object. */
+  virtual void setVelocity(const glm::vec2 &new_velocity) = 0;
+
+  /** @return Borders of the object in the game world used for collision detection. */
+  virtual ConvexPolygonView getBoundingPolygon() = 0;
+};
+} // namespace GameEngine
+
+#endif
