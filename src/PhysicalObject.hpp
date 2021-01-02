614ff1ec1b400588a0af7f78a0d9b3a801cfd49e
Alexander Heinrich <alxhnr@users.noreply.github.com>
2021-01-02 18:11:52 +0100

    Update documentation of PhysicalObject

--- src/PhysicalObject.hpp
+++ src/PhysicalObject.hpp
@@ -9,22 +9,22 @@
 #include <glm/vec2.hpp>
 
 namespace GameEngine {
+/** Represents an object which can move and collide with other objects. */
 class PhysicalObject {
 public:
   virtual ~PhysicalObject() = default;
 
-  /** Update the state of the object. This function does not need to apply the objects velocity to
-   * its position. This will be done by the physics integrator. Will be called only once per tick.
-   * Will be called once before all addVelocityStep() and handleCollisionWith() calls of this tick.
+  /** Update the state of the object for the current tick. This function does not need to apply the
+   * objects velocity to its position, which is done by the physics integrator. Will be called only
+   * once per tick, before all addVelocityStep() and handleCollisionWith() calls.
    */
   virtual void update() = 0;
 
   /** @return Current velocity of this object. Will be applied by the physics engine. */
   virtual const glm::vec2 &getVelocity() const = 0;
 
-  /** Move this object by the given amount. Must update the polygon returned by
-   * getBoundingPolygon(). May be called multiple times for multisampling during physics
-   * integration.
+  /** Move this object by the given amount. May be called multiple times for each tick to handle
+   * substeps. Must update the polygon returned by getBoundingPolygon().
    *
    * @param Offset Offset to be added to the objects current position.
    */
@@ -32,9 +32,10 @@ public:
 
   virtual const ConvexBoundingPolygon &getBoundingPolygon() const = 0;
 
-  /** Will be called if a collision occurred.
+  /** Will be called if a collision occurred. Two objects may collide multiple times during the same
+   * tick.
    *
-   * @param other Object which collided with this object.
+   * @param other Object which this object collided with.
    * @param displacement_vector Offset for moving this object out of the other object.
    */
   virtual void handleCollisionWith(PhysicalObject &other, const glm::vec2 &displacement_vector) = 0;
