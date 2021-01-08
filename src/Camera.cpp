9eb427ba2ff47d9afbfd4a79a29f9e743eeb6da8
Alexander Heinrich <alxhnr@users.noreply.github.com>
2021-01-08 12:58:00 +0100

    Add camera class

---
+++ src/Camera.cpp
@@ -0,0 +1,25 @@
+/** @file
+ * Implements a camera for transforming world coordinates to screen coordinates.
+ */
+
+#include "Camera.hpp"
+#include "glm/gtx/rotate_vector.hpp"
+
+namespace GameEngine {
+Camera::Camera(const size_t screen_width, const size_t screen_height)
+    : screen_center{screen_width / 2, screen_height / 2} {}
+
+glm::vec2 Camera::toScreenCoordinate(const glm::vec2 &world_coordinate) const {
+  return glm::rotate((world_coordinate - position) * scaling_factor, orientation) + screen_center;
+}
+
+glm::vec2 Camera::toWorldCoordinate(const glm::vec2 &screen_coordinate) const {
+  return glm::rotate(screen_coordinate - screen_center, -orientation) / scaling_factor + position;
+}
+
+void Camera::setPosition(const glm::vec2 &position) { this->position = position; }
+
+void Camera::setScale(float scaling_factor) { this->scaling_factor = scaling_factor; }
+
+void Camera::setOrientation(float orientation) { this->orientation = orientation; }
+} // namespace GameEngine
---
+++ src/Camera.hpp
@@ -0,0 +1,37 @@
+/** @file
+ * Contains a camera class.
+ */
+
+#ifndef GAME_ENGINE_SRC_CAMERA_HPP
+#define GAME_ENGINE_SRC_CAMERA_HPP
+
+#include <cstddef>
+#include <glm/vec2.hpp>
+
+namespace GameEngine {
+/** Transforms game world coordinates to screen coordinates. */
+class Camera {
+public:
+  Camera(size_t screen_width, size_t screen_height);
+
+  glm::vec2 toScreenCoordinate(const glm::vec2 &world_coordinate) const;
+  glm::vec2 toWorldCoordinate(const glm::vec2 &screen_coordinate) const;
+
+  /** @param position New center of the camera in the game world. */
+  void setPosition(const glm::vec2 &position);
+
+  /** @param scaling_factor E.g. 2.0f to zoom in by 2x. Defaults to 1.0f. */
+  void setScale(float scaling_factor);
+
+  /** @param orientation Rotation angle in radians. */
+  void setOrientation(float orientation);
+
+private:
+  glm::vec2 screen_center;
+  glm::vec2 position = {0, 0};
+  float scaling_factor = 1;
+  float orientation = 0;
+};
+} // namespace GameEngine
+
+#endif
